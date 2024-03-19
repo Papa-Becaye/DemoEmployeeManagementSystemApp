0eb5fc0772df93cf26e4114b1d5db2b2d46a961d
Papa Bécaye FALL <kountabecaye009@gmail.com>
2024-03-19 21:53:16 +0000

    Ajoutez des fichiers projet.

---
+++ BaseLibrary/DTOs/AccountDase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibrary.DTOs
+{
+    public class AccountDase
+    {
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [Required]
+
+        public string? Email { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+
+        public string? Password { get; set; }
+    }
+}
